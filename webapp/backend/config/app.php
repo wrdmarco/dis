f279817c9919792f93aa26b9ad8bb4cc7fd4f971
wrdmarco <marco@madeliefweide.nl>
2026-06-25 23:38:35 +0200

    Set application timezone to Amsterdam

--- webapp/backend/config/app.php
+++ webapp/backend/config/app.php
@@ -5,7 +5,7 @@ return [
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
     'url' => env('APP_URL', 'http://dis.example.nl'),
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Europe/Amsterdam'),
     'locale' => env('APP_LOCALE', 'nl'),
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
     'faker_locale' => env('APP_FAKER_LOCALE', 'nl_NL'),
